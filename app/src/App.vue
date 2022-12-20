9e152167bffc3c823ef634482f9a0a2955d61cee
Aunim HASSAN <aunim.hassan@gmail.com>
2022-12-20 20:14:41 +0100

    feat: done

---
+++ app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import GoalsList from './components/GoalsList.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <!-- <HelloWorld msg="Hello World!" /> -->
+      <GoalsList />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ app/src/components/GoalsList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const goals = ref([])
+
+const getGoals = () => {
+  const url = 'http://localhost:3030/api/goals/'
+  return fetch(url).then(response => response.json())
+}
+
+onMounted(() => {
+  getGoals().then(data => goals.value = data)
+})
+
+</script>
+
+<template>
+  <div class="goals">
+    <ul v-for="goal in goals">
+      <li>{{ goal.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
